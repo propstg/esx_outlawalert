23641b2ebf96c5349c157e7021e098575d5638ad
gregory propst <gregory.propst@gmail.com>
2019-01-06 02:15:03 -0500

    simplify logic a bit

--- client.lua
+++ client.lua
@@ -7,6 +7,29 @@ Citizen.CreateThread(function()
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
     end
+
+    local playerPed = GetPlayerPed(-1)
+
+    while true do
+        Wait(0)
+        if NetworkIsSessionStarted() then
+            DecorRegister("IsOutlaw",  3)
+            DecorSetInt(playerPed, "IsOutlaw", 1)
+            return
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    local playerPed = GetPlayerPed(-1)
+
+    while true do
+        Wait(0)
+        if DecorGetInt(playerPed, "IsOutlaw") == 2 then
+            Wait(math.ceil(timing))
+            DecorSetInt(playerPed, "IsOutlaw", 1)
+        end
+    end
 end)
 
 RegisterNetEvent('esx:playerLoaded')
@@ -21,128 +44,56 @@ end)
 
 RegisterNetEvent('outlawNotify')
 AddEventHandler('outlawNotify', function(alert)
-    if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+    if isPlayerPoliceOfficer() then
         Notify(alert)
-    end
-end)
-
-function Notify(text)
-    SetNotificationTextEntry('STRING')
-    AddTextComponentString(text)
-    DrawNotification(false, false)
-end
-
-Citizen.CreateThread(function()
-    while true do
-        Wait(0)
-        if NetworkIsSessionStarted() then
-            DecorRegister("IsOutlaw",  3)
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 1)
-            return
-        end
     end
 end)
 
 RegisterNetEvent('thiefPlace')
 AddEventHandler('thiefPlace', function(tx, ty, tz)
-    if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
-        if Config.CarJackingAlert then
-            local transT = 250
-            local thiefBlip = AddBlipForCoord(tx, ty, tz)
-            SetBlipSprite(thiefBlip,  10)
-            SetBlipColour(thiefBlip,  1)
-            SetBlipAlpha(thiefBlip,  transT)
-            SetBlipAsShortRange(thiefBlip,  1)
-            while transT ~= 0 do
-                Wait(Config.BlipJackingTime * 4)
-                transT = transT - 1
-                SetBlipAlpha(thiefBlip,  transT)
-                if transT == 0 then
-                    SetBlipSprite(thiefBlip,  2)
-                    return
-                end
-            end
-            
-        end
+    if Config.CarJackingAlert and isPlayerPoliceOfficer() then
+        showExpiringBlip(tx, ty, tz, 10, 1, Config.BlipJackingTime)
     end
 end)
 
 RegisterNetEvent('gunshotPlace')
 AddEventHandler('gunshotPlace', function(gx, gy, gz)
-    if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
-        if Config.GunshotAlert then
-            local transG = 250
-            local gunshotBlip = AddBlipForCoord(gx, gy, gz)
-            SetBlipSprite(gunshotBlip,  1)
-            SetBlipColour(gunshotBlip,  1)
-            SetBlipAlpha(gunshotBlip,  transG)
-            SetBlipAsShortRange(gunshotBlip,  1)
-            while transG ~= 0 do
-                Wait(Config.BlipGunTime * 4)
-                transG = transG - 1
-                SetBlipAlpha(gunshotBlip,  transG)
-                if transG == 0 then
-                    SetBlipSprite(gunshotBlip,  2)
-                    return
-                end
-            end
-        end
+    if Config.GunshotAlert and isPlayerPoliceOfficer() then
+        showExpiringBlip(gx, gy, gz, 1, 1, Config.BlipGunTime)
     end
 end)
 
 RegisterNetEvent('meleePlace')
 AddEventHandler('meleePlace', function(mx, my, mz)
-    if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
-        if Config.MeleeAlert then
-            local transM = 250
-            local meleeBlip = AddBlipForCoord(mx, my, mz)
-            SetBlipSprite(meleeBlip,  270)
-            SetBlipColour(meleeBlip,  17)
-            SetBlipAlpha(meleeBlip,  transG)
-            SetBlipAsShortRange(meleeBlip,  1)
-            while transM ~= 0 do
-                Wait(Config.BlipMeleeTime * 4)
-                transM = transM - 1
-                SetBlipAlpha(meleeBlip,  transM)
-                if transM == 0 then
-                    SetBlipSprite(meleeBlip,  2)
-                    return
-                end
-            end
-        end
-    end
-end)
-
-Citizen.CreateThread( function()
-    while true do
-        Wait(0)
-        if DecorGetInt(GetPlayerPed(-1), "IsOutlaw") == 2 then
-            Wait( math.ceil(timing) )
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 1)
-        end
+    if Config.MeleeAlert and isPlayerPoliceOfficer() then
+        showExpiringBlip(mx, my, mz, 270, 17, Config.BlipMeleeTime)
     end
 end)
 
 Citizen.CreateThread(function()
+    local playerPed = GetPlayerPed(-1)
+
     while true do
         Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
-        local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+        local plyPos = GetEntityCoords(playerPed,  true)
+        local s1, s2 = Citizen.InvokeNative(0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt())
         local street1 = GetStreetNameFromHashKey(s1)
         local street2 = GetStreetNameFromHashKey(s2)
-        if IsPedTryingToEnterALockedVehicle(GetPlayerPed(-1)) or IsPedJacking(GetPlayerPed(-1)) then
-            TriggerServerEvent('eden_garage:debug', "carjacking!")
+
+        if IsPedTryingToEnterALockedVehicle(playerPed) or IsPedJacking(playerPed) then
+            TriggerServerEvent('eden_garage:debug', 'carjacking!')
+
             Wait(3000)
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
-            local playerPed = GetPlayerPed(-1)
+
+            DecorSetInt(playerPed, 'IsOutlaw', 2)
+            
             local coords = GetEntityCoords(playerPed)
-            local vehicle = GetVehiclePedIsIn(playerPed,false)
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
             local vehicleProps = ESX.Game.GetVehicleProperties(vehicle)
-            if PlayerData.job ~= nil and PlayerData.job.name == 'police' and Config.ShowCopsMisbehave == false then
-            elseif PlayerData.job ~= nil and PlayerData.job.name == 'police' and Config.Config.ShowCopsMisbehave then
+
+            if Config.ShowCopsMisbehave and isPlayerPoliceOfficer() then
                 ESX.TriggerServerCallback('esx_outlawalert:ownvehicle',function(valid)
-                    if (valid) then
-                    else
+                    if not valid then
                         ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
                             local sex = nil
                             if skin.sex == 0 then
@@ -151,17 +102,17 @@ Citizen.CreateThread(function()
                                 sex = _('female')
                             end
                             TriggerServerEvent('thiefInProgressPos', plyPos.x, plyPos.y, plyPos.z)
-                            local veh = GetVehiclePedIsTryingToEnter(GetPlayerPed(-1))
+                            local veh = GetVehiclePedIsTryingToEnter(playerPed)
                             local vehName = GetDisplayNameFromVehicleModel(GetEntityModel(veh))
                             local vehName2 = GetLabelText(vehName)
                             if s2 == 0 then
-                                if IsPedInAnyPoliceVehicle(GetPlayerPed(-1)) then
+                                if IsPedInAnyPoliceVehicle(playerPed) then
                                     TriggerServerEvent('thiefInProgressS1police', street1, vehName2, sex)
                                 else
                                     TriggerServerEvent('thiefInProgressS1', street1, vehName2, sex)
                                 end
                             elseif s2 ~= 0 then
-                                if IsPedInAnyPoliceVehicle(GetPlayerPed(-1)) then
+                                if IsPedInAnyPoliceVehicle(playerPed) then
                                     TriggerServerEvent('thiefInProgressPolice', street1, street2, vehName2, sex)
                                 else
                                     TriggerServerEvent('thiefInProgress', street1, street2, vehName2, sex)
@@ -170,7 +121,7 @@ Citizen.CreateThread(function()
                         end)
                     end
                 end, vehicleProps)
-            else
+            elseif not isPlayerPoliceOfficer() then
                 ESX.TriggerServerCallback('esx_outlawalert:ownvehicle',function(valid)
                     if (valid) then
                     else
@@ -182,7 +133,7 @@ Citizen.CreateThread(function()
                                 sex = _('female')
                             end
                             TriggerServerEvent('thiefInProgressPos', plyPos.x, plyPos.y, plyPos.z)
-                            local veh = GetVehiclePedIsTryingToEnter(GetPlayerPed(-1))
+                            local veh = GetVehiclePedIsTryingToEnter(playerPed)
                             local vehName = GetDisplayNameFromVehicleModel(GetEntityModel(veh))
                             local vehName2 = GetLabelText(vehName)
                             if s2 == 0 then
@@ -198,17 +149,19 @@ Citizen.CreateThread(function()
     end
 end)
 
-Citizen.CreateThread( function()
+Citizen.CreateThread(function()
+    local playerPed = GetPlayerPed(-1)
+
     while true do
         Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        local plyPos = GetEntityCoords(playerPed,  true)
         local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
         local street1 = GetStreetNameFromHashKey(s1)
         local street2 = GetStreetNameFromHashKey(s2)
-        if IsPedInMeleeCombat(GetPlayerPed(-1)) then
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
-            if PlayerData.job ~= nil and PlayerData.job.name == 'police' and Config.ShowCopsMisbehave == false then
-            elseif PlayerData.job ~= nil and PlayerData.job.name == 'police' and Config.ShowCopsMisbehave then
+        
+        if IsPedInMeleeCombat(playerPed) then
+            DecorSetInt(playerPed, "IsOutlaw", 2)
+            if Config.ShowCopsMisbehave and isPlayerPoliceOfficer() then
                 ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
                     local sex = nil
                     if skin.sex == 0 then
@@ -224,7 +177,7 @@ Citizen.CreateThread( function()
                     end
                 end)
                 Wait(3000)
-            else
+            elseif not isPlayerPoliceOfficer() then
                 ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
                     local sex = nil
                     if skin.sex == 0 then
@@ -245,17 +198,20 @@ Citizen.CreateThread( function()
     end
 end)
 
-Citizen.CreateThread( function()
+Citizen.CreateThread(function()
+    local playerPed = GetPlayerPed(-1)
+
     while true do
         Wait(0)
-        local plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        local plyPos = GetEntityCoords(playerPed,  true)
         local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
         local street1 = GetStreetNameFromHashKey(s1)
         local street2 = GetStreetNameFromHashKey(s2)
-        if IsPedShooting(GetPlayerPed(-1)) then
-            DecorSetInt(GetPlayerPed(-1), "IsOutlaw", 2)
-            if PlayerData.job ~= nil and PlayerData.job.name == 'police' and Config.ShowCopsMisbehave == false then
-            elseif PlayerData.job ~= nil and PlayerData.job.name == 'police' and Config.ShowCopsMisbehave then
+
+        if IsPedShooting(playerPed) then
+            DecorSetInt(playerPed, "IsOutlaw", 2)
+
+            if Config.ShowCopsMisbehave and isPlayerPoliceOfficer() then
                 ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
                     local sex = nil
                     if skin.sex == 0 then
@@ -271,7 +227,7 @@ Citizen.CreateThread( function()
                     end
                 end)
                 Wait(3000)
-            else
+            elseif not isPlayerPoliceOfficer() then
                 ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
                     local sex = nil
                     if skin.sex == 0 then
@@ -291,3 +247,30 @@ Citizen.CreateThread( function()
         end
     end
 end)
+
+function Notify(text)
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
+
+function isPlayerPoliceOfficer()
+    return PlayerData.job ~= nil and PlayerData.job.name == 'police'
+end
+
+function showExpiringBlip(x, y, z, sprite, color, decayTime)
+    local transparency = 250
+    local blip = AddBlipForCoord(x, y, z)
+    SetBlipSprite(blip, sprite)
+    SetBlipColour(blip, color)
+    SetBlipAlpha(blip, transparency)
+    SetBlipAsShortRange(blip, 1)
+
+    while transparency > 0 do
+        Wait(decayTime * 4)
+        transparency = transparency - 1
+        SetBlipAlpha(blip, transparency)
+    end
+
+    SetBlipSprite(blip, 2)
+end
